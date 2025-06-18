7b2f7fd5611f557b235b471931668eddf2dc7aeb
alassiamailen <alassiamailen3@gmail.com>
2025-06-18 17:28:47 -0300

    [SIS-3] cree las clases Asaltante,BandaCriminal,Juez

---
+++ policia-federal/src/policia/federal/BandaCriminal.java
@@ -0,0 +1,27 @@
+package policia.federal;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author Alassia
+ */
+public class BandaCriminal {
+    double nro_de_identificacion;
+    double cantidad_de_miembros;
+
+    public BandaCriminal(double nro_de_identificacion, double cantidad_de_miembros) {
+        this.nro_de_identificacion = nro_de_identificacion;
+        this.cantidad_de_miembros = cantidad_de_miembros;
+    }
+    
+    public BandaCriminal(){
+        ingresarDatos();
+}
+    public void ingresarDatos(){
+        String aux_id, aux_cant_miembros;
+        double nro_de_identificacion, cantidad_de_miembros;
+        aux_id = JOptionPane.showInputDialog(null, "Ingrese numero de Identificacion de Banda Criminal: ");
+        nro_de_identificacion= Double.parseDouble(aux_id);
+        aux_cant_miembros=JOptionPane.showInputDialog(null,"Ingrese la cantidad de miembros que la componen");
+        cantidad_de_miembros= Double.parseDouble(aux_cant_miembros);
+    }
+}
